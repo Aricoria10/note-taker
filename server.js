28fb4334816955d5e2d6b3028444bd60d850a485
Aricoria10 <empresskitsune10@gmail.com>
2024-01-29 23:37:49 -0500

    commit before deploying to heroku

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
